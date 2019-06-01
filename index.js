f6ec007a3c820fcd7d66ef86decefce2ff50d7e1
Monica Lee <mylee.715@gmail.com>
2019-06-01 05:24:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ function prependKitten(name){
 }
 
 function removeLastKitten(name){
-  return kittens.slice(0, kittens.len)
+  return kittens.slice(0, kittens.length - 1);
 }
 
 function removeFirstKitten
