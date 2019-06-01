4b55aa01412a46d0f387a605679b724457f748dc
Monica Lee <mylee.715@gmail.com>
2019-06-01 04:54:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var kittens = ["Milo", "Otis", "Garfield"]; //define your array here
 
 // Add your functions and code here
-function destructivelyAppendKitten(){
-  return 
+function destructivelyAppendKitten(name){
+  return destructivelyAppendKitten.push("Ralph); 
 }
